e83e2db3aeb1d2b7140db9f519891bedc6946f3f
Saurabh Gupta <saurabhgupta.iitd@gmail.com>
2014-10-14 22:21:27 -0700

    added code for not recomputing..

--- semantic-segmentation/compute_map.m
+++ semantic-segmentation/compute_map.m
@@ -1,21 +1,26 @@
-function [map, dimensions] = compute_map(imName, paths, param, data)
+function [map, dimensions] = compute_map(imname, out_dir, param, data)
+  try
+    assert(~isempty(imname) && ~isempty(out_dir));
+    fileName = fullfile_ext(out_dir, imname, 'mat');
+    load(fileName, 'map', 'dimensions');
+  catch
+    switch(param.typ)
+      case 'colorSift',
+        % Call the binary to compute color sift here, for the time being loading features from disk here
+        % dt = load(fullfile('/work4/sgupta/kinect/iResultsNYU/bowmaps/siftK1000_2000/', strcat(imname, '.mat')));
+        % map = dt.bow_map;
+        [map] = sift(data.I, param.siftParam, data.vocab); 
 
-  switch(param.typ)
-    case 'colorSift',
-      % Call the binary to compute color sift here, for the time being loading features from disk here
-      % dt = load(fullfile('/work4/sgupta/kinect/iResultsNYU/bowmaps/siftK1000_2000/', strcat(imName, '.mat')));
-      % map = dt.bow_map;
-      [map] = sift(data.I, param.siftParam, data.vocab); 
+      case 'gTexton',
+        % f we want to compute the geocentric textons then compute them here
+        [map] = gTextons(data.pc, data.N, data.yDir, param);
 
-    case 'gTexton',
-      % f we want to compute the geocentric textons then compute them here
-      [map] = gTextons(data.pc, data.N, data.yDir, param);
+    end
+    dimensions = param.dimensions;
 
-  end
-  dimensions = param.dimensions;
-
-  if(~isempty(imName)),
-    fileName = fullfile(paths.mapDir, param.typ, strcat(imName, '.mat'));
-    save(fileName, 'map', 'dimensions');
+    if(~isempty(imname)),
+      fileName = fullfile(out_dir, strcat(imname, '.mat'));
+      save(fileName, 'map', 'dimensions');
+    end
   end
 end
