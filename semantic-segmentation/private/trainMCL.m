73cfb98d59b286ad137aabc59ca47603dc5f7143
Saurabh Gupta <saurabhgupta.iitd@gmail.com>
2014-10-19 14:45:40 -0700

    changes to the library for training svm classifier...

--- semantic-segmentation/private/trainMCL.m
+++ semantic-segmentation/private/trainMCL.m
@@ -1,8 +1,8 @@
 function model = trainMCL(x, y, w, C, bias, isWeighted) 
-	if(isWeighted == true),
+	if(isWeighted == false),
 		model = train(double(y'), sparse(x), ...
 			sprintf('-B %f -c %f -s %d -q',bias, C, 0),'col');
-	elseif(isWeighted == false),
+	elseif(isWeighted == true),
 		model = trainWt(double(w'), double(y'), sparse(x), ...
 			sprintf('-B %f -c %f -s %d -q',bias, C, 0),'col');
 	end
