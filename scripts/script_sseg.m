2ff2850c419c2c6af0c5480a7aa0fb32c307cfb5
Saurabh Gupta <saurabhgupta.iitd@gmail.com>
2014-10-19 15:21:09 -0700

    set up code for computing detector based features

--- scripts/script_sseg.m
+++ scripts/script_sseg.m
@@ -12,3 +12,67 @@ if strcmp(jobName, 'compute_ss_features')
   keyboard;
   [jobId jobDir] = jobParallel(jobName, resourceParam, jobParam, args);
 end
+
+if strcmp(jobName, 'ablationStudy')
+	classMapping = 'classMapping40';
+	trSet = 'train1';
+	valSet = 'train2';
+	testSet = 'val';
+	classifier = {'svm-full'};
+	typ = {'full', 'generic', 'categorySpecific'};
+
+	for i = 1:length(classifier),
+		for j = 1:length(typ), 
+			evalResAblation(i,j) = wrapperTrainTestBenchmarkModel(trSet, valSet, testSet, typ{j}, classifier{i}, classMapping);
+			fprintf('%s - %s, accuracy found to be, %0.3f.\n', classifier{i}, typ{j}, evalResAblation(i,j).fwavacc);
+		end
+	end
+end
+
+if strcmp(jobName, 'detectors_for_sseg')
+  res = rcnn_all('task-detection', 'rgb_hha', 'trainval', 'test');
+  res = rcnn_all('task-detection', 'rgb_hha', 'train', 'val');
+  res = rcnn_all('task-detection', 'rgb_hha', 'val', 'train');
+end
+
+if strcmp(jobName, 'copy_boxes')
+  p = get_paths();
+  
+  s = sprintf('cp %s/detections/*boxes*test*.mat %s/. \n', fullfile(p.detection_dir, 'detector', 'rgb_hha_30000_trainval'), p.ss_det_dir); 
+  fprintf(s); 
+  % system(s);
+  
+  s = sprintf('cp %s/detections/*boxes*train*.mat %s/. \n', fullfile(p.detection_dir, 'detector', 'rgb_hha_30000_val'), p.ss_det_dir); 
+  fprintf(s); 
+  % system(s);
+  
+  s = sprintf('cp %s/detections/*boxes*val*.mat %s/. \n', fullfile(p.detection_dir, 'detector', 'rgb_hha_30000_train'), p.ss_det_dir); 
+  fprintf(s); 
+  % system(s);
+end
+
+if strcmp(jobName, 'deepDetFeatures')
+  imSet = {'train', 'val', 'test'};
+  typ = 'detInstV4';
+  catName = getMetadata('task-detection'); catName = catName.name;
+  detDir = p.ss_det_dir; nmsThresh = 0.3;
+  dirNameSuffix = 'detection_'; 
+  assignTyp = {'bestScore'};
+  pThresh = 0.50;
+  threshSet = 'val'; boxFieldName = 'boxes';
+  K = 1; args = {};
+  for i = 1:length(imSet),
+    for j = 1:length(assignTyp),
+      for k = 1:length(pThresh),
+        for l = 1:K,
+          imlist = getImageSet(imSet{i});
+          args{end+1} = {imSet{i}, ucmThresh, typ, l:K:length(imlist), catName, detDir, assignTyp{j}, pThresh(k), threshSet, dirNameSuffix, nmsThresh, boxFieldName};
+        end
+      end
+    end
+  end
+  jobParam = struct('numThreads', 1, 'codeDir', pwd(), 'preamble', '', 'matlabpoolN', 4, 'globalVars', {{}}, 'fHandle', @wrapperComputeFeatures4, 'numOutputs', 0);
+  resourceParam = struct('mem', 4, 'hh', 8, 'numJobs', -1, 'ppn', 4, 'nodes', 1, 'logDir', '/work4/sgupta/pbsBatchDir/', 'queue', 'psi', 'notif', false, 'username', 'sgupta', 'headNode', 'zen');
+  jobName = 'detObj'; 
+  [jobId jobDir] = jobParallel(jobName, resourceParam, jobParam, args);
+end
