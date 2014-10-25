d2c0f51394c8c6fed0f46e5616fb827519871c43
Saurabh Gupta <saurabhgupta.iitd@gmail.com>
2014-10-24 17:23:55 -0700

    retab
    Signed-off-by: Saurabh Gupta <saurabhgupta.iitd@gmail.com>

--- rcnn/process_region_candidates.m
+++ rcnn/process_region_candidates.m
@@ -1,70 +1,70 @@
 function [region_meta_info]=preprocess_mcg_candidates(imnames, mcgdir, gtdir, ovoutdir, sptextdir, regspimgdir, N)
 if(~exist(ovoutdir, 'file'))
-    mkdir(ovoutdir); 
+  mkdir(ovoutdir); 
 end
 if(~exist(sptextdir, 'file'))
-    mkdir(sptextdir); 
+  mkdir(sptextdir); 
 end
 if(~exist(regspimgdir, 'file'))
-    mkdir(regspimgdir); 
+  mkdir(regspimgdir); 
 end
 
 
 
 for i=1:numel(imnames)
-    tmp=load(fullfile(mcgdir, [imnames{i} '.mat']));
-    candidates=tmp.candidates; clear tmp;
-    %get reg2sp
-    sp=candidates.superpixels;
-    N1=min(N, numel(candidates.labels));
-    reg2sp=false(max(sp(:)),N1);
-    for j=1:N1
-        reg2sp(candidates.labels{j},j)=true;
-    end
-    
-    %compress to remove irrelevant sps
-    [sp, reg2sp]=compressSp2reg(sp, reg2sp);
+  tmp=load(fullfile(mcgdir, [imnames{i} '.mat']));
+  candidates=tmp.candidates; clear tmp;
+  %get reg2sp
+  sp=candidates.superpixels;
+  N1=min(N, numel(candidates.labels));
+  reg2sp=false(max(sp(:)),N1);
+  for j=1:N1
+    reg2sp(candidates.labels{j},j)=true;
+  end
+  
+  %compress to remove irrelevant sps
+  [sp, reg2sp]=compressSp2reg(sp, reg2sp);
 
-    if(~exist(fullfile(ovoutdir, [imnames{i} '.mat']), 'file'))
-    %compute overlaps between everything
-    ov=double(reg2sp')*double(reg2sp);
-    ov=ov>0;
-    save(fullfile(ovoutdir, [imnames{i} '.mat']), 'ov');
-    end
-    %load gt
-    if(isempty(gtdir))
-        %no gt
-        inst=zeros(size(sp));
-        categories=[];
-    else
-    [cls, inst, categories]=load_gt(gtdir, imnames{i});
-    end
-    %compute overlaps with ground truth
-    overlap=get_gt_overlaps(reg2sp, sp, inst);    
-    box_overlap=get_gt_overlaps_box(reg2sp, sp, inst);
-    %populate meta info
-    region_meta_info.num_regs(i)=size(reg2sp,2)+numel(categories);
-    region_meta_info.overlaps{i}=overlap;
-    region_meta_info.box_overlaps{i}=box_overlap;
-    region_meta_info.gt{i}=[categories(:)' zeros(1,size(reg2sp,2))];
-    
-    %add ground truth to reg2sp
-    gt_sp=double(inst+1);
-    gt_reg2sp=[zeros(1,numel(categories)); eye(numel(categories))];
-    gt_reg2sp=logical(gt_reg2sp);
-    [sp, reg2sp]=intersect_partitions(gt_sp, sp, gt_reg2sp, reg2sp);
-    region_meta_info.boxes{i}=get_region_boxes(sp, reg2sp);
-    %write sprep
-    
-    textfile=fullfile(sptextdir, [imnames{i} '.txt']);
-    if(~exist(textfile, 'file'))
-    write_sprep_text(sp, textfile);
-    reg2spfile=fullfile(regspimgdir, [imnames{i} '.png']);
-    imwrite(uint8(reg2sp),reg2spfile);
-    end
-    fprintf('Done %d\n', i);
+  if(~exist(fullfile(ovoutdir, [imnames{i} '.mat']), 'file'))
+  %compute overlaps between everything
+  ov=double(reg2sp')*double(reg2sp);
+  ov=ov>0;
+  save(fullfile(ovoutdir, [imnames{i} '.mat']), 'ov');
+  end
+  %load gt
+  if(isempty(gtdir))
+    %no gt
+    inst=zeros(size(sp));
+    categories=[];
+  else
+  [cls, inst, categories]=load_gt(gtdir, imnames{i});
+  end
+  %compute overlaps with ground truth
+  overlap=get_gt_overlaps(reg2sp, sp, inst);  
+  box_overlap=get_gt_overlaps_box(reg2sp, sp, inst);
+  %populate meta info
+  region_meta_info.num_regs(i)=size(reg2sp,2)+numel(categories);
+  region_meta_info.overlaps{i}=overlap;
+  region_meta_info.box_overlaps{i}=box_overlap;
+  region_meta_info.gt{i}=[categories(:)' zeros(1,size(reg2sp,2))];
+  
+  %add ground truth to reg2sp
+  gt_sp=double(inst+1);
+  gt_reg2sp=[zeros(1,numel(categories)); eye(numel(categories))];
+  gt_reg2sp=logical(gt_reg2sp);
+  [sp, reg2sp]=intersect_partitions(gt_sp, sp, gt_reg2sp, reg2sp);
+  region_meta_info.boxes{i}=get_region_boxes(sp, reg2sp);
+  %write sprep
+  
+  textfile=fullfile(sptextdir, [imnames{i} '.txt']);
+  if(~exist(textfile, 'file'))
+  write_sprep_text(sp, textfile);
+  reg2spfile=fullfile(regspimgdir, [imnames{i} '.png']);
+  imwrite(uint8(reg2sp),reg2spfile);
+  end
+  fprintf('Done %d\n', i);
 end
-        
+      
 
 function write_sprep_text(sp, filename)
   fid=fopen(filename, 'w');
