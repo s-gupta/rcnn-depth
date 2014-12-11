9012213548c6ec04eea3663e9221175cc3787160
Saurabh Gupta <saurabhgupta.iitd@gmail.com>
2014-12-11 10:14:45 -0800

    added warnings for non-existant directories and how to get them.

--- startup.m
+++ startup.m
@@ -1,5 +1,9 @@
 function startup()
   % genPath = {'piotr-structured-edges-pami', 'rgbdutils', 'utils'};
+  if(~exist('nyu-hooks', 'dir')), warning('Missing nyu-hooks. You will need it for some functions. Get it from here: https://github.com/s-gupta/nyu-hooks'); end
+  if(~exist('utils', 'dir')), warning('Missing utils. You will need it for some fuctions. Get it from here: https://github.com/s-gupta/utils/'); end
+  if(~exist('rgbdutils', 'dir')), warning('Missing rgbd-utils. You will need it for some functions. Get it from here: https://github.com/s-gupta/rgbdutils'); end
+
   addPath = {'nyu-hooks', 'scripts', 'mcg', 'utils', 'caffe/matlab/caffe/', 'rgbdutils'};
   genPath = {'structured-edges', 'rcnn'};
 
